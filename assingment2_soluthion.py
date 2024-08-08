313d0a29693f58265ac624403486e9dabefa2669
maliktariqkhakhi <muhammadtariqmaliktariq176@gmail.com>
2024-08-08 07:53:31 -0700

    assingment 1 solution

---
+++ assingment2_soluthion.py
@@ -0,0 +1,69 @@
+message:str = ("Today weather is very beautiful")
+print(message)
+
+message_2:str = ("I like typing of python")
+print(message_2)
+message_2 = ("python is easy to learn")
+print(message_2)
+name:str = "malik tariq"
+print(f"Hello {name}, would you like to learn some python today?")
+
+name:str = "malik mahad"
+print(f"in lower case {name.lower()}")
+print(f"in upper case {name.upper()}")
+print(f"in title cae {name.title()}")
+print('''Albert Einstein once said, “A person who never made a mistake never
+tried anything new." ''')
+famous_person :str= "Albert Einstein"
+message:str=' "A person who never made a mistake never tried anything new."'
+print(f"{famous_person} once  said,{message}")
+name: str = " \n\t malik triq\n\t"
+print(name)
+print("\nlstrip():")
+print(name.lstrip())
+print("\nrstrip():")
+print(name.rstrip())
+print("strip function")
+print(name.strip())
+x:int = 5
+y:int = 10
+z:int =15
+print(f"sum of x,y,z={x+y+z}")
+a:int =5
+b:int =10
+c:int =a
+a=b
+b=c
+print("a=",a)
+print("b=",b)
+favorite_colour ="white"
+print(f"My favorite colour is:{favorite_colour}")
+
+colour_favorite="white"
+print(f"My favorite colour is still:{colour_favorite} ")
+pet_name:str= "Buddy"
+pet_name = "tomi"
+print(pet_name)
+Sunshine:str = "Sunshine"
+print(Sunshine)
+score:int=100
+print(score)
+score:int=150
+print(score)
+city:str ="islamabad"
+print (city)
+text:str ="python programming"
+print(text.title())
+mixed:str = "Malik tariq"
+print(mixed.lower())
+print(mixed.upper())
+temperature:int=25
+print(f"The current temperature is {temperature}degrees.")
+poem:str = """
+
+The sun sets slow and paints the sky,
+A fiery hue that makes me sigh.
+The stars come out and twinkle bright,
+A night of rest, a peaceful sight.
+"""
+print( poem)
