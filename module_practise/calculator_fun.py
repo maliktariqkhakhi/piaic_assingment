169a0ce97e4212ee54f7033401bab00e01b87b3e
maliktariqkhakhi <muhammadtariqmaliktariq176@gmail.com>
2024-09-23 07:44:56 -0700

    vnbn

---
+++ module_practise/calculator_fun.py
@@ -0,0 +1,15 @@
+def add_number (first_value,second_value)->float:
+    print(f"the sum of {first_value}, and {second_value} is:")
+    return first_value + second_value
+
+def subtract_number (first_value,second_value)->float:
+    print(f"the sum of {first_value},and {second_value} is:")
+    return first_value - second_value
+
+def multiply_number (first_value, second_value)->float:
+    print(f"the sum of {first_value},and{second_value} is:")
+    return first_value * second_value
+
+def divide_number (first_value, second_value)->float:
+    print(f"the sum of {first_value},and {second_value} is:")
+    return first_value / second_value
---
+++ module_practise/module_fun.py
@@ -0,0 +1,8 @@
+def greeting(name:str)-> str:
+    print(f"Hello {name}")
+    
+
+
+malik:str= "malik tariq"
+
+variable_numbers: int = 123
